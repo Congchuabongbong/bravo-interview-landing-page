b71df67f8632bfb57574ec0719137383116f656c
Nguyễn Văn Bình <binhvnth2011019@fpt.edu.vn>
2022-07-16 18:13:47 +0700

    next commit

--- src/app/pages/article/article.component.ts
+++ src/app/pages/article/article.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { ParseXmlToObjPipe } from 'src/app/pipe/parse-xml-to-obj.pipe';
 import { ApiServiceService, MediaTypes } from 'src/app/services/api-service.service';
+import { ArticleServiceService, Article } from 'src/app/services/article-service.service';
 @Component({
     selector: 'app-article',
     templateUrl: './article.component.html',
     styleUrls: ['./article.component.css'],
 })
 export class ArticleComponent implements OnInit {
-    constructor(private apiService: ApiServiceService, private parseXmlToObj: ParseXmlToObjPipe) {}
+    public articleList: Article[] = [];
+    constructor(
+        private apiService: ApiServiceService,
+        private parseXmlToObj: ParseXmlToObjPipe,
+        private articleService: ArticleServiceService,
+    ) {}
 
     ngOnInit(): void {
         this.getArticle();
@@ -15,6 +21,6 @@ export class ArticleComponent implements OnInit {
     public async getArticle(): Promise<any> {
         let dataXML: any = await this.apiService.getData('assets/data.xml', MediaTypes.XML);
         let dataObj: any = this.parseXmlToObj.transform(dataXML);
-        console.log(dataObj.RSS.CHANNEL[0].ITEM[0].DESCRIPTION[0].OL[0].LI[0].A[0].$.HREF);
+        this.articleList = this.articleService.formatArticleFromData(dataObj.RSS.CHANNEL[0].ITEM);
     }
 }
--- src/app/services/article-service.service.ts
+++ src/app/services/article-service.service.ts
@@ -1,8 +1,31 @@
 import { Injectable } from '@angular/core';
-export interface Article {}
+export interface Article extends Source {
+    pubDate: Date;
+    source: Source;
+}
+type Source = {
+    title: string;
+    link: string;
+};
 @Injectable({
     providedIn: 'root',
 })
 export class ArticleServiceService {
     constructor() {}
+
+    public formatArticleFromData(items: any[]): any {
+        let articleList: Article[] = items.map((item) => {
+            let article: Article = {
+                link: item.LINK[0],
+                title: item.TITLE[0],
+                pubDate: item.PUBDATE[0],
+                source: {
+                    title: item.SOURCE[0]._,
+                    link: item.SOURCE[0].$.URL,
+                },
+            };
+            return article;
+        });
+        return articleList;
+    }
 }
