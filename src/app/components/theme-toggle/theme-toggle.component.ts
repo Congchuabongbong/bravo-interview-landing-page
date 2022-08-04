13d2967aad575a16306f1ff11464852115552d5a
Nguyễn Văn Bình <binhvnth2011019@fpt.edu.vn>
2022-08-04 16:38:56 +0700

    next commit

--- src/app/components/theme-toggle/theme-toggle.component.ts
+++ src/app/components/theme-toggle/theme-toggle.component.ts
@@ -1,25 +1,36 @@
-import { Component, OnInit, Renderer2 } from '@angular/core';
+import { Component, OnInit, Renderer2, ElementRef } from '@angular/core';
+import { LocalStorageService } from '../../services/local-storage.service';
 @Component({
-  selector: 'app-theme-toggle',
-  templateUrl: './theme-toggle.component.html',
-  styleUrls: ['./theme-toggle.component.css']
+    selector: 'app-theme-toggle',
+    templateUrl: './theme-toggle.component.html',
+    styleUrls: ['./theme-toggle.component.css'],
 })
 export class ThemeToggleComponent implements OnInit {
-
-  constructor(private renderer: Renderer2) { }
-
-  ngOnInit(): void {
-  }
-  public switchTheme(e: any) {
-    if (e.target.checked) {
-        this.renderer.addClass(document.documentElement, 'dark');
-        this.renderer.removeClass(document.documentElement, 'light');
-        // localStorage.setItem('theme', 'dark');
-    } else {
-        this.renderer.addClass(document.documentElement, 'light');
-        this.renderer.removeClass(document.documentElement, 'dark');
-        // this.elementRef.nativeElement.documentElement.setAttribute('class', 'light');
-        // localStorage.setItem('theme', 'light');
+    constructor(
+        private renderer: Renderer2,
+        private localStorageService: LocalStorageService,
+        private elementRef: ElementRef,
+    ) {}
+    private currentTheme!: any;
+    ngOnInit(): void {
+        this.currentTheme = this.localStorageService.getItem('theme');
+        if (this.currentTheme) {
+            this.renderer.addClass(document.documentElement, this.currentTheme);
+            if (this.currentTheme === 'dark') {
+                console.log('dark');
+                this.elementRef.nativeElement.querySelector('.toggle-switch input[type="checkbox"]').checked = true;
+            }
+        }
     }
-}
+    public switchTheme(e: any) {
+        if (e.target.checked) {
+            this.renderer.addClass(document.documentElement, 'dark');
+            this.renderer.removeClass(document.documentElement, 'light');
+            this.localStorageService.setItem('theme', 'dark');
+        } else {
+            this.renderer.addClass(document.documentElement, 'light');
+            this.renderer.removeClass(document.documentElement, 'dark');
+            this.localStorageService.setItem('theme', 'light');
+        }
+    }
 }
--- src/app/services/local-storage.service.ts
+++ src/app/services/local-storage.service.ts
@@ -5,4 +5,30 @@ import { Injectable } from '@angular/core';
 })
 export class LocalStorageService {
     constructor() {}
+    public getItem(itemName: string): any {
+        const item = localStorage.getItem(itemName);
+        const numPatt = new RegExp(/^\d+$/);
+        const jsonPatt = new RegExp(/[\[\{].*[\}\]]/);
+        if (item) {
+            if (jsonPatt.test(item)) {
+                return JSON.parse(item);
+            } else if (numPatt.test(item)) {
+                return parseFloat(item);
+            } else {
+                return item;
+            }
+        } else {
+            return null;
+        }
+    }
+    public setItem(itemName: string, item: any) {
+        if (typeof item === 'object') {
+            localStorage.setItem(itemName, JSON.stringify(item));
+        } else {
+            localStorage.setItem(itemName, item);
+        }
+    }
+    public remove(itemName: string) {
+        localStorage.removeItem(itemName);
+    }
 }
